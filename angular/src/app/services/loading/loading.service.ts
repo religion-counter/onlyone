ca50710aeccb627986e91279e57fc57e0b7837eb
religion-counter <TodorBonchev@Todors-MacBook-Pro.local>
2021-11-05 15:04:13 +0200

    remove old angular folder

--- angular/src/app/services/loading/loading.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-
-@Injectable({ providedIn: 'root' })
-export class LoadingService  {
-
-    public isLoading = new BehaviorSubject(false);
-    constructor() { }  
-}
